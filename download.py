1b06a828f4796b9a740525cec8b7985cad217273
NguyenNgocThanhSang <nguyensang2032003@gmail.com>
2025-04-01 15:59:37 +0700

    add vncorenlp to change to PhoBERT model

--- download.py
+++ download.py
@@ -1,6 +1,10 @@
 import py_vncorenlp
 
-save_dir = 'models/vncorenlp/'
+save_dir = './models/vncorenlp'
 
-py_vncorenlp.download_model(save_dir=save_dir)
+# py_vncorenlp.download_model(save_dir=save_dir)
 
+rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=save_dir)
+text = "Ông Nguyễn Khắc Chúc  đang làm việc tại Đại học Quốc gia Hà Nội. Bà Lan, vợ ông Chúc, cũng làm việc tại đây."
+output = rdrsegmenter.word_segment(text)
+print(output)
